5688721886e672b71d8c4722eab6ff5369ffa964
ke2933 <534144810@qq.com>
2018-09-11 18:54:22 +0800

    添加图片压缩

--- imgCounsel/ImgCounsel.js
+++ imgCounsel/ImgCounsel.js
@@ -1,4 +1,78 @@
 $(function () {
+    // 图片压缩 ES6 canvas
+    function reduceImg(base64) {
+        return new Promise(function (resolve, reject) {
+            var canvas = document.createElement('canvas');
+            var context = canvas.getContext('2d');
+            var img = new Image();
+            img.src = base64;
+            // base64地址图片加载完毕后
+            img.onload = function () {
+                // 图片原始尺寸
+                var originWidth = this.width;
+                var originHeight = this.height;
+                // 最大尺寸限制，可通过国设置宽高来实现图片压缩程度
+                var maxWidth = 800,
+                    maxHeight = 800;
+                // 目标尺寸
+                var targetWidth = originWidth,
+                    targetHeight = originHeight;
+                // 图片尺寸超过400x400的限制
+                if (originWidth > maxWidth || originHeight > maxHeight) {
+                    if (originWidth / originHeight > maxWidth / maxHeight) {
+                        // 更宽，按照宽度限定尺寸
+                        targetWidth = maxWidth;
+                        targetHeight = Math.round(maxWidth * (originHeight / originWidth));
+                    } else {
+                        targetHeight = maxHeight;
+                        targetWidth = Math.round(maxHeight * (originWidth / originHeight));
+                    }
+                }
+                // canvas对图片进行缩放
+                canvas.width = targetWidth;
+                canvas.height = targetHeight;
+                // 清除画布
+                context.clearRect(0, 0, targetWidth, targetHeight);
+                // 图片压缩
+                context.drawImage(img, 0, 0, targetWidth, targetHeight);
+                /*第一个参数是创建的img对象；第二个参数是左上角坐标，后面两个是画布区域宽高*/
+                //压缩后的图片base64 url
+                /*canvas.toDataURL(mimeType, qualityArgument),mimeType 默认值是'image/jpeg';
+                 * qualityArgument表示导出的图片质量，只要导出为jpg和webp格式的时候此参数才有效果，默认值是0.92*/
+                //base64 格式
+                resolve(canvas.toDataURL('image/jpeg', 0.92));
+            }
+        })
+    }
+
+    // base64 转 file 对象
+    function dataURLtoFile(dataurl, filename) {//将base64转换为文件  
+        var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+        while (n--) {
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new File([u8arr], filename, { type: mime });
+    }
+
+    // file 压缩
+    function reduceFile(file) {
+        return new Promise(function (resolve, reject) {
+            // file 转 base64
+            var reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = function (e) {
+                if (e.target.result) {
+                    // base64 进行压缩
+                    reduceImg(e.target.result).then(function (newBase64) {
+                        // 压缩后的base64 再 转回 file
+                        resolve(dataURLtoFile(newBase64, file.name))
+                    })
+                }
+            }
+        })
+    }
+
     var goodsInfo = myLocal.getItem("goodsInfo");
     if (myLocal.getItem("patientInfo")) {
         var patientInfo = myLocal.getItem("patientInfo");
@@ -22,8 +96,7 @@ $(function () {
     })
     // 选择患者-end
 
-
-    var fileAllArr = [];
+    // 所有图片url数组
     var imgUrlArr = [];
     // 文件 处理 - start
     $(".fileInput").change(function () {
@@ -35,89 +108,60 @@ $(function () {
             shadeClose: false,
             skin: 'noBackground',
         });
-        var newFileArr = [];
-        var uploadFile = $(this)[0].files; // 某一块添加时的原始数据
-        var fileLength = 0;
-        var reader = new FileReader();
-        reader.readAsDataURL(uploadFile[fileLength]);
-        reader.onload = function (e) {
-            if (e.target.result) {
-                // 过滤重复
-                var flag = true;
-                for (var i = 0; i < fileAllArr.length; i++) {
-                    if (fileAllArr[i].name == uploadFile[fileLength].name) {
-                        flag = false;
-                    }
-                }
-                if (flag) {
-                    if (/(.png|.jpg|.jpeg|.pdf)$/gi.test(uploadFile[fileLength].name)) {
-                        fileAllArr.push({
-                            "name": uploadFile[fileLength].name,
-                            "value": uploadFile[fileLength],
-                        });
-                        newFileArr.push(uploadFile[fileLength]);
-                    } else {
-                        layer.msg('请上传png/jpg/jpeg类型的文件');
-                    }
-                } else {
-                    // layer.msg('重复文件已过滤');
-                }
-                fileLength++;
-                if (fileLength < uploadFile.length) {
-                    reader.readAsDataURL(uploadFile[fileLength]);
-                } else {
-                    var postData = new FormData();
-                    postData.append("recordType", "patient-record");
-                    for (var i = 0; i < newFileArr.length; i++) {
-                        postData.append("files", newFileArr[i]);
-                    }
-                    $.ajax({
-                        headers: {
-                            token: myLocal.getItem("token"),
-                        },
-                        type: 'POST',
-                        url: IP + '/api-third/cos/uploads',
-                        xhrFields: {
-                            withCredentials: true,
-                        },
-                        crossDomain: true,
-                        processData: false,
-                        contentType: false,
-                        data: postData,
-                        dataType: 'json',
-                        success: function (data) {
-                            console.log(data)
-                            if (data.code == 20000) {
-                                layer.closeAll();
-                                $(".loadingContainer").hide();
-                                var tempArr = data.result;
-                                imgUrlArr = imgUrlArr.concat(tempArr);
-                                var _html = '';
-                                for (var i = 0; i < tempArr.length; i++) {
-                                    _html += '<li class="imgItem" url="' + tempArr[i] + '">\
-                                    <img class="photo" src="'+ tempArr[i] + '" alt="">\
-                                    <img class="deleteImg" src="../images/delete.png" alt="">\
-                                </li>'
-                                };
-                                $(".imgList").append(_html)
-                            } else {
+        var files = $(this)[0].files;// 选择的图片资源 
+        for (var i = 0; i < files.length; i++) {
+            reduceFile(files[i]).then(function (newFile) {
+                var postData = new FormData();
+                postData.append("recordType", "patient-record");
+                postData.append("files", newFile);
+                $.ajax({
+                    headers: {
+                        token: myLocal.getItem("token"),
+                    },
+                    type: 'POST',
+                    url: IP + '/api-third/cos/uploads',
+                    xhrFields: {
+                        withCredentials: true,
+                    },
+                    crossDomain: true,
+                    processData: false,
+                    contentType: false,
+                    data: postData,
+                    dataType: 'json',
+                    success: function (data) {
+                        console.log(data)
+                        layer.closeAll();
+                        $(".loadingContainer").hide();
+                        if (data.code == 20000) {
+                            var tempArr = data.result;
+                            imgUrlArr = imgUrlArr.concat(tempArr);
+                            var _html = '';
+                            for (var i = 0; i < tempArr.length; i++) {
+                                _html += '<li class="imgItem" url="' + tempArr[i] + '">\
+                                <img class="photo" src="'+ tempArr[i] + '" alt="">\
+                                <img class="deleteImg" src="../images/delete.png" alt="">\
+                            </li>'
+                            };
+                            $(".imgList").append(_html)
+                        } else {
 
-                            }
-                        },
-                        error: function (err) {
-                            console.log(err);
-                        },
-                    })
-                }
-            }
+                        }
+                    },
+                    error: function (err) {
+                        console.log(err);
+                    },
+                })
+            })
         }
     })
+
     // 预览图片
     $(".imgList").delegate(".imgItem", "click", function () {
         var urls = [];
         for (var i = 0; i < $(".imgList .imgItem").length; i++) {
             urls.push($(".imgList .imgItem").eq(i).attr("url"))
         }
+        console.log(urls)
         wx.previewImage({
             current: $(this).attr('url'), // 当前显示图片的http链接
             urls: urls, // 需要预览的图片http链接列表
@@ -131,7 +175,7 @@ $(function () {
                 token: myLocal.getItem("token"),
             },
             type: 'POST',
-            url: IP + '/api-third/cos/delete',
+            url: IP + '/api-archive/medical/report/deleteInquiryReport',
             xhrFields: {
                 withCredentials: true,
             },
--- orderCompile/OrderCompile.js
+++ orderCompile/OrderCompile.js
@@ -1,4 +1,78 @@
 $(function () {
+    // 图片压缩 ES6 canvas
+    function reduceImg(base64) {
+        return new Promise(function (resolve, reject) {
+            var canvas = document.createElement('canvas');
+            var context = canvas.getContext('2d');
+            var img = new Image();
+            img.src = base64;
+            // base64地址图片加载完毕后
+            img.onload = function () {
+                // 图片原始尺寸
+                var originWidth = this.width;
+                var originHeight = this.height;
+                // 最大尺寸限制，可通过国设置宽高来实现图片压缩程度
+                var maxWidth = 800,
+                    maxHeight = 800;
+                // 目标尺寸
+                var targetWidth = originWidth,
+                    targetHeight = originHeight;
+                // 图片尺寸超过400x400的限制
+                if (originWidth > maxWidth || originHeight > maxHeight) {
+                    if (originWidth / originHeight > maxWidth / maxHeight) {
+                        // 更宽，按照宽度限定尺寸
+                        targetWidth = maxWidth;
+                        targetHeight = Math.round(maxWidth * (originHeight / originWidth));
+                    } else {
+                        targetHeight = maxHeight;
+                        targetWidth = Math.round(maxHeight * (originWidth / originHeight));
+                    }
+                }
+                // canvas对图片进行缩放
+                canvas.width = targetWidth;
+                canvas.height = targetHeight;
+                // 清除画布
+                context.clearRect(0, 0, targetWidth, targetHeight);
+                // 图片压缩
+                context.drawImage(img, 0, 0, targetWidth, targetHeight);
+                /*第一个参数是创建的img对象；第二个参数是左上角坐标，后面两个是画布区域宽高*/
+                //压缩后的图片base64 url
+                /*canvas.toDataURL(mimeType, qualityArgument),mimeType 默认值是'image/jpeg';
+                 * qualityArgument表示导出的图片质量，只要导出为jpg和webp格式的时候此参数才有效果，默认值是0.92*/
+                //base64 格式
+                resolve(canvas.toDataURL('image/jpeg', 0.92));
+            }
+        })
+    }
+
+    // base64 转 file 对象
+    function dataURLtoFile(dataurl, filename) {//将base64转换为文件  
+        var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+        while (n--) {
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new File([u8arr], filename, { type: mime });
+    }
+
+    // file 压缩
+    function reduceFile(file) {
+        return new Promise(function (resolve, reject) {
+            // file 转 base64
+            var reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = function (e) {
+                if (e.target.result) {
+                    // base64 进行压缩
+                    reduceImg(e.target.result).then(function (newBase64) {
+                        // 压缩后的base64 再 转回 file
+                        resolve(dataURLtoFile(newBase64, file.name))
+                    })
+                }
+            }
+        })
+    }
+
     // 获取订单详情-start
     var orderId = myLocal.getItem("orderId");
     var patientInfo = null;// 患者信息
@@ -6,7 +80,7 @@ $(function () {
     var orderInfo = null;// 订单信息
     $.ajax({
         headers: {
-            token:myLocal.getItem("token"),
+            token: myLocal.getItem("token"),
         },
         type: 'POST',
         url: IP + '/api-order/order/inquiry/getOrder',
@@ -122,7 +196,7 @@ $(function () {
             },
         })
     }
-    getBaseInfoById(myLocal.getItem("patientInfo").id);
+    myLocal.getItem("patientInfo") && getBaseInfoById(myLocal.getItem("patientInfo").id);
 
     // 选择患者
     $(".selectBtn").click(function () {
@@ -141,84 +215,61 @@ $(function () {
         });
     })
 
-    var fileAllArr = [];
     var imgUrlArr = [];
     // 文件 处理 - start
     $(".fileInput").change(function () {
-        var newFileArr = [];
-        var uploadFile = $(this)[0].files; // 某一块添加时的原始数据
-        var _html = '';
-        var fileLength = 0;
-        var reader = new FileReader();
-        reader.readAsDataURL(uploadFile[fileLength]);
-        reader.onload = function (e) {
-            if (e.target.result) {
-                // 过滤重复
-                var flag = true;
-                for (var i = 0; i < fileAllArr.length; i++) {
-                    if (fileAllArr[i].name == uploadFile[fileLength].name) {
-                        flag = false;
-                    }
-                }
-                if (flag) {
-                    if (/(.png|.jpg|.jpeg|.pdf)$/gi.test(uploadFile[fileLength].name)) {
-                        fileAllArr.push({
-                            "name": uploadFile[fileLength].name,
-                            "value": uploadFile[fileLength],
-                        });
-                        newFileArr.push(uploadFile[fileLength]);
-                    } else {
-                        layer.msg('请上传png/jpg/jpeg类型的文件');
-                    }
-                } else {
-                    // layer.msg('重复文件已过滤');
-                }
-                fileLength++;
-                if (fileLength < uploadFile.length) {
-                    reader.readAsDataURL(uploadFile[fileLength]);
-                } else {
-                    var postData = new FormData();
-                    postData.append("recordType", "patient-record");
-                    for (var i = 0; i < newFileArr.length; i++) {
-                        postData.append("files", newFileArr[i]);
-                    }
-                    $.ajax({
-                        headers: {
-                            token: myLocal.getItem("token"),
-                        },
-                        type: 'POST',
-                        url: IP + '/api-third/cos/uploads',
-                        xhrFields: {
-                            withCredentials: true,
-                        },
-                        crossDomain: true,
-                        processData: false,
-                        contentType: false,
-                        data: postData,
-                        dataType: 'json',
-                        success: function (data) {
-                            console.log(data)
-                            if (data.code == 20000) {
-                                var tempArr = data.result;
-                                imgUrlArr = imgUrlArr.concat(tempArr);
-                                var _html = '';
-                                for (var i = 0; i < tempArr.length; i++) {
-                                    _html += '<li class="imgItem" url="' + tempArr[i] + '">\
-                                    <img class="photo" src="'+ tempArr[i] + '" alt="">\
-                                    <img class="deleteImg" src="../images/delete.png" alt="">\
-                                </li>'
-                                };
-                                $(".imgList").append(_html)
-                            } else {
+        layer.open({
+            title: '',
+            type: 1,
+            content: $('.loadingContainer'),
+            closeBtn: false,
+            shadeClose: false,
+            skin: 'noBackground',
+        });
+        var files = $(this)[0].files;// 选择的图片资源 
+        for (var i = 0; i < files.length; i++) {
+            reduceFile(files[i]).then(function (newFile) {
+                var postData = new FormData();
+                postData.append("recordType", "patient-record");
+                postData.append("files", newFile);
+                $.ajax({
+                    headers: {
+                        token: myLocal.getItem("token"),
+                    },
+                    type: 'POST',
+                    url: IP + '/api-third/cos/uploads',
+                    xhrFields: {
+                        withCredentials: true,
+                    },
+                    crossDomain: true,
+                    processData: false,
+                    contentType: false,
+                    data: postData,
+                    dataType: 'json',
+                    success: function (data) {
+                        console.log(data)
+                        layer.closeAll();
+                        $(".loadingContainer").hide();
+                        if (data.code == 20000) {
+                            var tempArr = data.result;
+                            imgUrlArr = imgUrlArr.concat(tempArr);
+                            var _html = '';
+                            for (var i = 0; i < tempArr.length; i++) {
+                                _html += '<li class="imgItem" url="' + tempArr[i] + '">\
+                                <img class="photo" src="'+ tempArr[i] + '" alt="">\
+                                <img class="deleteImg" src="../images/delete.png" alt="">\
+                            </li>'
+                            };
+                            $(".imgList").append(_html)
+                        } else {
 
-                            }
-                        },
-                        error: function (err) {
-                            console.log(err);
-                        },
-                    })
-                }
-            }
+                        }
+                    },
+                    error: function (err) {
+                        console.log(err);
+                    },
+                })
+            })
         }
     })
 
@@ -231,7 +282,7 @@ $(function () {
                     token: myLocal.getItem("token"),
                 },
                 type: 'POST',
-                url: IP + '/api-third/cos/delete',
+                url: IP + '/api-archive/medical/report/deleteInquiryReport',
                 xhrFields: {
                     withCredentials: true,
                 },
@@ -275,7 +326,7 @@ $(function () {
                     token: myLocal.getItem("token"),
                 },
                 type: 'POST',
-                url: IP + '/api-archive/medical/report/addInquiryReport',
+                url: IP + '/api-archive/medical/report/updateInquiryReport',
                 xhrFields: {
                     withCredentials: true,
                 },
@@ -283,6 +334,7 @@ $(function () {
                 data: {
                     "patientId": patientInfo.id,
                     "imgUrlArr": tempImgUrls.join(','),
+                    "reportGroup": orderInfo.patientArchive,
                 },
                 dataType: 'json',
                 success: function (data) {
